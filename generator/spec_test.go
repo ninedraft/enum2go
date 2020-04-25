1f55a0700b1af672b020dc360a39c0564f7e3292
Pavel Petrukhin <pp@kodix.ru>
2020-04-25 20:06:40 +0300

    (generator enum spe): add basic tests for enumeration spec methods

---
+++ generator/spec_test.go
@@ -0,0 +1,52 @@
+package generator
+
+import (
+	"go/ast"
+	"go/format"
+	"go/parser"
+	"go/token"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestSpec(test *testing.T) {
+	var fset = token.NewFileSet()
+	var filter = func(file os.FileInfo) bool {
+		return true
+	}
+	var pkgs, errPars = parser.ParseDir(fset, "cast", filter, parser.AllErrors)
+	if errPars != nil {
+		test.Fatal(errPars)
+	}
+	var pkg *ast.Package
+	for _, p := range pkgs {
+		pkg = p
+	}
+	var files []*ast.File
+	for _, file := range pkg.Files {
+		files = append(files, file)
+	}
+	var cast = &ast.File{}
+	mergeFiles(cast, files)
+
+	var spec = enumSpec{
+		Package:  "result",
+		Type:     "Kind",
+		Kind:     kindInt,
+		Format:   kebab,
+		BaseType: ast.NewIdent("int"),
+		Names: []*ast.Ident{
+			ast.NewIdent("AaA"),
+			ast.NewIdent("bbA"),
+			ast.NewIdent("C"),
+		},
+	}
+	var gen = &Generator{
+		typePlaceholder: "Θ",
+	}
+	spec.Pour(gen, cast)
+	if err := format.Node(ioutil.Discard, fset, cast); err != nil {
+		test.Fatal(err)
+	}
+}
