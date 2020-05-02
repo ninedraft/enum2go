0383ffe9774fd3e4ed830acc7a988c249269c3c8
Pavel Petrukhin <pp@kodix.ru>
2020-05-02 20:32:20 +0300

    feat(project): add generator for static data

---
+++ mage.go
@@ -0,0 +1,65 @@
+// +build mage
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"go/ast"
+	"go/format"
+	"go/parser"
+	"go/token"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/ninedraft/enum2go/pkg/astx"
+
+	"go.uber.org/multierr"
+)
+
+func Generate() error {
+	return multierr.Combine(
+		embeddedCast(),
+	)
+}
+
+func embeddedCast() error {
+	var fset = token.NewFileSet()
+	var castPkgDir = filepath.Join("pkg", "generator", "enumcast")
+	log.Printf("parsing template package %q", castPkgDir)
+
+	var pkgs, errParseDir = parser.ParseDir(fset, castPkgDir, filterCastFiles, parser.AllErrors)
+	if errParseDir != nil {
+		return fmt.Errorf("unable to parse template package %q: %v", castPkgDir, errParseDir)
+	}
+	var pkg, errSelectPackage = astx.SelectPkg(pkgs)
+	if errSelectPackage != nil {
+		return fmt.Errorf("unable to select template package: %v", errSelectPackage)
+	}
+	var files = astx.PkgFiles(pkg)
+	var c = &ast.File{
+		Name: ast.NewIdent("cast"),
+	}
+	astx.MergeFiles(c, files)
+
+	var data = &bytes.Buffer{}
+	_ = format.Node(data, fset, c)
+
+	var result = bytes.NewBufferString(
+		"// code generate by mage script. DO NOT EDIT.\n\n" +
+			"package static\n\n" +
+			fmt.Sprintf("import %q\n", "github.com/ninedraft/enum2go/pkg/generator/cast") +
+			fmt.Sprintf("import %q\n\n", "bytes"),
+	)
+	_, _ = fmt.Fprintf(result, "var Cast = cast.MustFromRe(%q, bytes.NewReader(%#v))\n\n", "cast", data.Bytes())
+	return ioutil.WriteFile("pkg/static/cast.go", result.Bytes(), 0755)
+}
+
+func filterCastFiles(file os.FileInfo) bool {
+	var isNotTest = !strings.HasSuffix(file.Name(), "_test.go")
+	var isNotManifest = file.Name() != "pkg.go"
+	return isNotTest && isNotManifest
+}
